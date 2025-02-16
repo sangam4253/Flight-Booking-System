4369e9b05849022bde503b5a024003dd43e904d3
Sangam4253 <sangamkumar4253@gmail.com>
2025-02-16 23:46:09 +0530

    intial commit

---
+++ Flight_Backend/src/main/java/Sangam/com/Flight_Backend/FlightBackendApplication.java
@@ -0,0 +1,13 @@
+package Sangam.com.Flight_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightBackendApplication.class, args);
+	}
+
+}
---
+++ Flight_Backend/src/test/java/Sangam/com/Flight_Backend/FlightBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package Sangam.com.Flight_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
